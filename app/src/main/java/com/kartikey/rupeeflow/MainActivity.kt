6235728cf3740ba7a43491da37f02ef6900c0898
itskartik51 <kartikey.toda@gmail.com>
2026-07-09 11:28:34 +0530

    Update MainActivity.kt

--- app/src/main/java/com/kartikey/rupeeflow/MainActivity.kt
+++ app/src/main/java/com/kartikey/rupeeflow/MainActivity.kt
@@ -7,18 +7,20 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+// Yahan humne aapki banayi hui UI_Screens folder se HomeScreen ko bula liya
+import com.kartikey.rupeeflow.UI_Screens.HomeScreen 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            // Humara Mint/Lavender theme yahan se call hoga
             MaterialTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    // Yahan hum app ki Main Screen (Home/Assets) load karenge
+                    // Ab app khulte hi seedha aapka banaya hua Dashboard/Form dikhega!
+                    HomeScreen()
                 }
             }
         }
