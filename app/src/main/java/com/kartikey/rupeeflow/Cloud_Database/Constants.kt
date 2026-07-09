6d7ef5994230cd04f79317f91129abf74642ec03
itskartik51 <kartikey.toda@gmail.com>
2026-07-09 10:20:28 +0530

    Create Constants.kt

---
+++ app/src/main/java/com/kartikey/rupeeflow/Cloud_Database/Constants.kt
@@ -0,0 +1,7 @@
+package com.kartikey.rupeeflow.Cloud_Database
+
+object Constants {
+    // Yahan aapki Google Sheet (App Script) ka link aayega.
+    // Future mein koi nayi sheet ya email use karni ho, toh bas is link ko change karna hoga.
+    const val GOOGLE_SHEET_API_URL = "YOUR_GOOGLE_SHEET_LINK_HERE"
+}
