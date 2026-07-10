c305a2c6fd62cb9eb6844fd4cbd9e6dd0c9772fb
itskartik51 <kartikey.toda@gmail.com>
2026-07-10 08:28:56 +0530

    Create StockButton.kt

---
+++ app/src/main/java/com/kartikey/rupeeflow/UI_Screens/Features/StockButton.kt
@@ -0,0 +1,26 @@
+package com.kartikey.rupeeflow.UI_Screens.Features
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun StockButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier.height(50.dp),
+        shape = RoundedCornerShape(12.dp),
+        // Green Theme as per our design
+        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF2E7D32)) 
+    ) {
+        Text(text = text, color = Color.White)
+    }
+}
