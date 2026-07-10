584f71471dd9e7953f473d86646e675b6c91d74a
itskartik51 <kartikey.toda@gmail.com>
2026-07-10 10:56:15 +0530

    Update and rename GridCard.kt to GridCard.kt

--- app/src/main/java/com/kartikey/rupeeflow/UI_Screens/Home/GridCard.kt
+++ app/src/main/java/com/kartikey/rupeeflow/UI_Screens/Home/GridCard.kt
@@ -1,4 +1,4 @@
-package com.kartikey.rupeeflow.UI_Screens.Features
+package com.kartikey.rupeeflow.UI_Screens.Home
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
