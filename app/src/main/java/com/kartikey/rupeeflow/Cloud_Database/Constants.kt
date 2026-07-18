2f595313b93e1848d5d6c08df95d92223d00b01d
itskartik51 <kartikey.toda@gmail.com>
2026-07-18 08:25:35 +0530

    Update Constants.kt

--- app/src/main/java/com/kartikey/rupeeflow/Cloud_Database/Constants.kt
+++ app/src/main/java/com/kartikey/rupeeflow/Cloud_Database/Constants.kt
@@ -4,23 +4,37 @@ object Constants {
     const val GOOGLE_SHEET_API_URL = "https://script.google.com/macros/s/AKfycbxq6GiNwSEDI85iWu8Zl1mMokr0TPidrIKyM5eHfnnPumnhq4Z1szCLjg-JcuTK_gr6Aw/exec"
 
     val IndianBanksList = listOf(
+        // Core Banks
         "State Bank of India (SBI)", "HDFC Bank", "ICICI Bank", "Axis Bank", "Kotak Mahindra Bank",
         "Punjab National Bank (PNB)", "Bank of Baroda", "Bank of India", "Union Bank of India",
         "Canara Bank", "Central Bank of India", "Indian Bank", "Indian Overseas Bank", "UCO Bank",
         "Bank of Maharashtra", "IDBI Bank", "Yes Bank", "IndusInd Bank", "Federal Bank",
-        "South Indian Bank", "IDFC First Bank", "Bandhan Bank", "RBL Bank", "AU Small Finance Bank",
-        "Equitas Small Finance Bank", "Ujjivan Small Finance Bank", "Utkarsh Small Finance Bank",
-        "Paytm Payments Bank", "Airtel Payments Bank", "India Post Payments Bank", "Jio Payments Bank",
-        "Fino Payments Bank", "Standard Chartered Bank", "Citi Bank", "HSBC Bank", "DBS Bank",
-        "Barclays Bank", "Deutsche Bank", "J&K Bank", "Karnataka Bank", "Karur Vysya Bank",
-        "City Union Bank", "Dhanlaxmi Bank", "CSB Bank", "Nainital Bank", "Tamilnad Mercantile Bank",
-        "Suryoday Small Finance Bank", "Shivalik Small Finance Bank", "Capital Small Finance Bank",
-        "Jana Small Finance Bank", "Unity Small Finance Bank", "ESAF Small Finance Bank",
-        "North East Small Finance Bank", "Saraswat Cooperative Bank", "Cosmos Cooperative Bank", "Other Bank"
+        "South Indian Bank", "IDFC First Bank", "Bandhan Bank", "RBL Bank",
+        
+        // Small Finance & Cooperative Banks
+        "AU Small Finance Bank", "Equitas Small Finance Bank", "Ujjivan Small Finance Bank",
+        "Utkarsh Small Finance Bank", "Suryoday Small Finance Bank", "Shivalik Small Finance Bank",
+        "Capital Small Finance Bank", "Jana Small Finance Bank", "Unity Small Finance Bank",
+        "ESAF Small Finance Bank", "North East Small Finance Bank", "Saraswat Cooperative Bank", 
+        "Cosmos Cooperative Bank",
+        
+        // Payments Banks
+        "Paytm Payments Bank", "Airtel Payments Bank", "India Post Payments Bank", 
+        "Jio Payments Bank", "Fino Payments Bank",
+        
+        // Foreign & Regional Banks
+        "Standard Chartered Bank", "Citi Bank", "HSBC Bank", "DBS Bank", "Barclays Bank", 
+        "Deutsche Bank", "J&K Bank", "Karnataka Bank", "Karur Vysya Bank", "City Union Bank", 
+        "Dhanlaxmi Bank", "CSB Bank", "Nainital Bank", "Tamilnad Mercantile Bank",
+        
+        // NEW: Fintechs, Co-Branded & Credit Card Issuers
+        "Amazon Pay", "Flipkart", "KIWI", "Super.money", "OneCard", "Slice", "Uni Cards",
+        "Tata Neu", "Swiggy", "Zomato", "SBI Card", "Cred", "Other Bank"
     )
 
     // 100% Pure Root Domains for Maximum API Hit-Rate
     val BankDomainMap = mapOf(
+        // Banks
         "State Bank of India (SBI)" to "sbi.co.in", "HDFC Bank" to "hdfcbank.com",
         "ICICI Bank" to "icicibank.com", "Axis Bank" to "axisbank.com", "Kotak Mahindra Bank" to "kotak.com",
         "Punjab National Bank (PNB)" to "pnbindia.in", "Bank of Baroda" to "bankofbaroda.in",
@@ -45,6 +59,21 @@ object Constants {
         "Jana Small Finance Bank" to "janabank.com", "Unity Small Finance Bank" to "theunitybank.com",
         "ESAF Small Finance Bank" to "esafbank.com", "North East Small Finance Bank" to "nesfb.com",
         "Saraswat Cooperative Bank" to "saraswatbank.com", "Cosmos Cooperative Bank" to "cosmosbank.com",
+        
+        // NEW: Fintechs, Co-Branded & Credit Card Issuers Domains
+        "Amazon Pay" to "amazon.in",
+        "Flipkart" to "flipkart.com",
+        "KIWI" to "gokiwi.in",
+        "Super.money" to "super.money",
+        "OneCard" to "getonecard.app",
+        "Slice" to "sliceit.com",
+        "Uni Cards" to "uni.cards",
+        "Tata Neu" to "tatadigital.com",
+        "Swiggy" to "swiggy.com",
+        "Zomato" to "zomato.com",
+        "SBI Card" to "sbicard.com",
+        "Cred" to "cred.club",
+        
         "Other Bank" to "rbi.org.in"
     )
 }
