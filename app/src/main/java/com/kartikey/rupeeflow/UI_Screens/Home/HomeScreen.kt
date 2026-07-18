588ba5fb99727ae539ad0afb1fe98f925cf6cef2
itskartik51 <kartikey.toda@gmail.com>
2026-07-18 08:28:23 +0530

    Update HomeScreen.kt

--- app/src/main/java/com/kartikey/rupeeflow/UI_Screens/Home/HomeScreen.kt
+++ app/src/main/java/com/kartikey/rupeeflow/UI_Screens/Home/HomeScreen.kt
@@ -74,7 +74,7 @@ fun HomeDashboardDesign(
                 
                 Constants.IndianBanksList.forEach { bankName ->
                     val domain = Constants.BankDomainMap[bankName] ?: "rbi.org.in"
-                    val googleLogoUrl = "https://www.google.com/s2/favicons?domain=$domain&sz=128"
+                    val googleLogoUrl = "https://www.google.com/s2/favicons?domain=$domain&sz=512"
                     
                     Row(
                         verticalAlignment = Alignment.CenterVertically,
