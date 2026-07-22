d34ddc988fd090465c05a540f8dc821be00d9d1b
itskartik51 <kartikey.toda@gmail.com>
2026-07-22 10:04:01 +0530

    Update Contri.kt

--- app/src/main/java/com/kartikey/rupeeflow/UI_Screens/Home/Contri.kt
+++ app/src/main/java/com/kartikey/rupeeflow/UI_Screens/Home/Contri.kt
@@ -1,6 +1,7 @@
 package com.kartikey.rupeeflow.UI_Screens.Home
 
 import androidx.compose.animation.*
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
