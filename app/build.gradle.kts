f2cfc5a1a63eff9c57bc69409d9b98577889ad25
itskartik51 <kartikey.toda@gmail.com>
2026-07-31 10:19:12 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.kartikey.rupeeflow"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0.0"
+        versionCode = 4
+        versionName = "1.00.01"
     }
 
     signingConfigs {
