5d7beba3f356b81a07c666e7f4406e2dac5b1709
itskartik51 <kartikey.toda@gmail.com>
2026-08-24 16:28:18 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 12
-        versionName = "1.00.012"
+        versionName = "1.2405.012"
         
         // OPTIMIZATION: Removes 80+ unused languages (Spanish, French, etc.) added by Firebase/Google Play Services
         resourceConfigurations += listOf("en", "hi")
